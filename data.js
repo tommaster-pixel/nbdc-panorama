42d7a420191086cc47ebbed6f1e03885f0a95aef
tommaster-pixel <tomjerry360degree@gmail.com>
2021-01-04 13:58:14 +0530

    add all files

---
+++ data.js
@@ -0,0 +1,248 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-second-mahall-1-view",
+      "name": "second mahall 1 view",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 750,
+      "initialViewParameters": {
+        "yaw": 0.07589025180876163,
+        "pitch": 0.10479647981475182,
+        "fov": 1.3687812585745385
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.22084251391039444,
+          "pitch": 0.0009477935021529049,
+          "rotation": 0,
+          "target": "1-second-mahall-2-view"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.3808767112687796,
+          "pitch": -0.24188596862676803,
+          "title": "mirror",
+          "text": "this is a ultra modern mirror with cross groove"
+        },
+        {
+          "yaw": 1.0464892363332066,
+          "pitch": 0.027670814538204525,
+          "title": "window",
+          "text": "this is a window flower grill ss work"
+        }
+      ]
+    },
+    {
+      "id": "1-second-mahall-2-view",
+      "name": "second mahall 2 view",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 750,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.22731717829414855,
+          "pitch": -0.022917839408156837,
+          "rotation": 0,
+          "target": "2-second-mahall-3-view"
+        },
+        {
+          "yaw": 2.9756913335011017,
+          "pitch": 0.21232897376189896,
+          "rotation": 0,
+          "target": "0-second-mahall-1-view"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-second-mahall-3-view",
+      "name": "second mahall 3 view",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 750,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.17611525899130598,
+          "pitch": -0.10736557300921135,
+          "rotation": 0,
+          "target": "3-second-mahall-4-view"
+        },
+        {
+          "yaw": -2.609407312703329,
+          "pitch": 0.02885609865550265,
+          "rotation": 0,
+          "target": "1-second-mahall-2-view"
+        },
+        {
+          "yaw": 1.9716681778914413,
+          "pitch": -0.027080304133663802,
+          "rotation": 0,
+          "target": "4-second-mahall-5-view"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-second-mahall-4-view",
+      "name": "second mahall 4 view",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 750,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.18903323543774597,
+          "pitch": 0.44620403269858677,
+          "rotation": 0,
+          "target": "2-second-mahall-3-view"
+        },
+        {
+          "yaw": -0.20025325325777565,
+          "pitch": 0.1902624078910673,
+          "rotation": 0,
+          "target": "1-second-mahall-2-view"
+        },
+        {
+          "yaw": -0.10006022129099179,
+          "pitch": 0.05550672033599113,
+          "rotation": 0,
+          "target": "0-second-mahall-1-view"
+        },
+        {
+          "yaw": -1.139446310099821,
+          "pitch": 0.002244818581500496,
+          "rotation": 0,
+          "target": "4-second-mahall-5-view"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-second-mahall-5-view",
+      "name": "second mahall 5 view",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 750,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.5990569591484203,
+          "pitch": 0.01303942080495446,
+          "rotation": 0,
+          "target": "3-second-mahall-4-view"
+        },
+        {
+          "yaw": -0.1233403127709547,
+          "pitch": 0.21747593165461865,
+          "rotation": 0,
+          "target": "2-second-mahall-3-view"
+        },
+        {
+          "yaw": -0.992460895225058,
+          "pitch": 0.20646884246630748,
+          "rotation": 0,
+          "target": "1-second-mahall-2-view"
+        },
+        {
+          "yaw": -1.3615259190012559,
+          "pitch": 0.08130044916220847,
+          "rotation": 0,
+          "target": "0-second-mahall-1-view"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "mahall view",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
